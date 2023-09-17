9d763ee85809be82f4e12e8410d7586b5a6be75c
Afranioalves <afranio777alves@gmail.com>
2023-09-17 13:23:45 +0100

    chore: Adicionar novas dependecias

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useCallback} from 'react';
+import { View } from "react-native";
+import Route from "./src/routers";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+const App = () =>{
+
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Mukta-Bold": require("./assets/fonts/Mukta-Bold.ttf"),
+    "Mukta-Regular": require("./assets/fonts/Mukta-Regular.ttf"),
+    "Mukta-SemiBold": require("./assets/fonts/Mukta-SemiBold.ttf"),
+    "Mukta-Medium": require("./assets/fonts/Mukta-Medium.ttf"),
+    "Mukta-ExtraBold": require("./assets/fonts/Mukta-ExtraBold.ttf"),
+   
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <View onLayout={onLayoutRootView} style={{flex:1}}><Route /></View>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
