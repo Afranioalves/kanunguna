811a59c3e05eacfca81fa53a5fe4160c3e1acef2
Afranioalves <afranio777alves@gmail.com>
2023-09-19 23:58:07 +0100

    chore: Adicionar fake data

---
+++ src/data/category.data.js
@@ -0,0 +1,258 @@
+import Beer from '../../assets/img/beer.jpg'
+import Computer from '../../assets/img/computer.jpeg'
+import Electro from '../../assets/img/electro.jpg'
+import Fruit from '../../assets/img/fruit.jpg'
+import Milk from '../../assets/img/milk.jpg'
+import Refris from '../../assets/img/refris.png'
+import Smartphone from '../../assets/img/smartphone.jpg'
+import Sound from '../../assets/img/sound.jpg'
+import Tv from '../../assets/img/tv.jpg'
+import Water from '../../assets/img/water.jpg'
+
+export const categories = [
+    {
+
+        id:1,
+        name:'Frutas e Legumes',
+        image:Fruit,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Cove'
+            },
+            {
+                id:3,
+                name:'Maça'
+            },
+            {
+                id:4,
+                name:'Tomate'
+            }
+        ]
+    },
+
+    {
+
+        id:2,
+        name:'Leites',
+        image:Milk,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Nido'
+            },
+            {
+                id:3,
+                name:'Cowbell'
+            },
+            {
+                id:4,
+                name:'Loya'
+            }
+        ]
+    },
+
+    {
+
+        id:3,
+        name:'Refrigerantes',
+        image:Refris,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Coca cola'
+            },
+            {
+                id:3,
+                name:'Pespi'
+            },
+            {
+                id:4,
+                name:'Fanta'
+            }
+        ]
+    },
+
+    {
+
+        id:4,
+        name:'Cerveja',
+        image:Beer,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Cuca'
+            },
+            {
+                id:3,
+                name:'Nocal'
+            },
+            {
+                id:4,
+                name:'Booster'
+            }
+        ]
+    },
+    {
+
+        id:5,
+        name:'Electro-doméstico',
+        image:Electro,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Fogão'
+            },
+            {
+                id:3,
+                name:'Microondas'
+            },
+            {
+                id:4,
+                name:'Torradeira'
+            }
+        ]
+    },
+
+    {
+
+        id:6,
+        name:'TV',
+        image:Tv,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Samsung'
+            },
+            {
+                id:3,
+                name:'Phillipe'
+            },
+            {
+                id:4,
+                name:'Sharp'
+            }
+        ]
+    },
+    {
+
+        id:7,
+        name:'Som',
+        image:Sound,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'JBL'
+            },
+            {
+                id:3,
+                name:'Phillipe'
+            },
+            {
+                id:4,
+                name:'Sharp'
+            }
+        ]
+    },
+    {
+
+        id:8,
+        name:'Smatphone',
+        image:Smartphone,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Xiaomi'
+            },
+            {
+                id:3,
+                name:'Apple'
+            },
+            {
+                id:4,
+                name:'Samsung'
+            }
+        ]
+    },
+    {
+
+        id:9,
+        name:'Água',
+        image:Water,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Pura'
+            },
+            {
+                id:3,
+                name:'Perl'
+            },
+            {
+                id:4,
+                name:'Orion'
+            }
+        ]
+    },
+    {
+
+        id:10,
+        name:'Computadores',
+        image:Computer,
+        subCategory:[
+            {
+                id:1,
+                name:'Todos'
+            },
+            {
+                id:2,
+                name:'Nootbook'
+            },
+            {
+                id:3,
+                name:'Laptops'
+            },
+            {
+                id:4,
+                name:'Zenbook'
+            }
+        ]
+    },
+    
+]
---
+++ src/data/product.data.js
@@ -0,0 +1,63 @@
+import image_1 from '../../assets/img/product_1.png'
+import image_2 from '../../assets/img/product_2.png'
+import image_3 from '../../assets/img/product_3.png'
+import image_4 from '../../assets/img/product_4.png'
+import image_5 from '../../assets/img/product_5.png'
+import image_6 from '../../assets/img/product_6.png'
+
+export const products = [
+    {
+        name:'Saodimallsu Womens Turtleneck Oversized',
+        priceNow:2000,
+        priceOld:3000,
+        stars:4,
+        discount:-40,
+        liked:true,
+        image:image_1
+    },
+    {
+        name:'Womens Turtleneck Oversized',
+        priceNow:7000,
+        priceOld:9000,
+        stars:2,
+        discount:40,
+        liked:false,
+        image:image_2
+    },
+    {
+        name:'Turtleneck Oversized Oversized',
+        priceNow:2000,
+        priceOld:0,
+        stars:4,
+        discount:0,
+        liked:true,
+        image:image_3
+    },
+    {
+        name:'Saodimallsu Womens Oversized',
+        priceNow:4000,
+        priceOld:3000,
+        stars:1,
+        discount:-40,
+        liked:false,
+        image:image_4
+    },
+    {
+        name:'Womens Turtleneck Oversized',
+        priceNow:2000,
+        priceOld:3000,
+        stars:4,
+        discount:-40,
+        liked:false,
+        image:image_5
+    },
+    {
+        name:'Saodimallsu Womens Turtleneck Oversized',
+        priceNow:2000,
+        priceOld:3000,
+        stars:4,
+        discount:-40,
+        liked:false,
+        image:image_6
+    }
+] 
